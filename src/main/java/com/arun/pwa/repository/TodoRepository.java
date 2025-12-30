67095a6f2347e1d2552505bbc37bb70f36288c1b
Arunkumar C <arunkumar061781@gmail.com>
2025-12-30 12:23:18 +0530

    Version 3

---
+++ src/main/java/com/arun/pwa/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.arun.pwa.repository;
+
+import com.arun.pwa.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
